4ee4a2ee8c54094688c6d859b1a18aa78f3fd8e4
mrgong0515 <mrgong0515@gmail.com>
2026-02-11 21:04:04 +0900

    [Bronze V] Title: 개, Time: 0 ms, Memory: 1112 KB -BaekjoonHub

---
+++ 백준/Bronze/10172. 개/개.c
@@ -0,0 +1,12 @@
+#include<stdio.h>
+
+int main(void)
+{
+    printf("%c%c_/%c\n", 124, 92, 124);
+    printf("%cq p%c   /}\n", 124, 124);
+    printf("%c 0 %c%c%c%c%c\n", 40, 41, 34, 34, 34, 92);
+    printf("%c%c^%c%c    %c\n", 124, 34, 34, 96, 124);
+    printf("%c%c_/=%c%c__%c\n", 124, 124, 92, 92, 124);
+    
+    return 0;
+}
