6053e1c5ca064d410c983a8736e9dffed180dfea
mrgong0515 <mrgong0515@gmail.com>
2026-02-11 20:54:57 +0900

    [Bronze V] Title: 고양이, Time: 0 ms, Memory: 1112 KB -BaekjoonHub

---
+++ 백준/Bronze/10171. 고양이/고양이.c
@@ -0,0 +1,11 @@
+#include<stdio.h>
+
+int main()
+{
+    printf("%c    /%c\n", 92, 92);
+    printf(" )  ( ')\n");
+    printf("(  /  )\n");
+    printf(" %c(__)%c\n", 92, 124);
+    
+    return 0;
+}
