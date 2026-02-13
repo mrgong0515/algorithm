297982d087622ab8a6196c15a836eb4ecac43e8e
mrgong0515 <mrgong0515@gmail.com>
2026-02-13 17:35:36 +0900

    [Bronze V] Title: 사분면 고르기, Time: 0 ms, Memory: 1116 KB -BaekjoonHub

--- 백준/Bronze/14681. 사분면 고르기/사분면 고르기.c
+++ 백준/Bronze/14681. 사분면 고르기/사분면 고르기.c
@@ -8,16 +8,16 @@ int main(void)
     {
         printf("1");
     }
-        else if( x < 0 && y > 0)
-        {
-            printf("2");
-        }
-        else if( x < 0 && y < 0)
-            {
-                printf("3");
-            }
-         else
-             printf("4");
+    else if( x < 0 && y > 0)
+    {
+         printf("2");
+    }
+     else if( x < 0 && y < 0)
+    {
+         printf("3");
+     }
+     else
+          printf("4");
     
     return 0;
 }
