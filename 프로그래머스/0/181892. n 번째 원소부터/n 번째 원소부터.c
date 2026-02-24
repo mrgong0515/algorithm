f3b29f7dbfb87a8dc6312fc9529453f0d2207a52
공성우 <mrgong0515@gmail.com>
2026-02-24 18:16:13 +0900

    [level 0] Title: n 번째 원소부터, Time: 0.02 ms, Memory: 4.2 MB -BaekjoonHub

--- 프로그래머스/0/181892. n 번째 원소부터/n 번째 원소부터.c
+++ 프로그래머스/0/181892. n 번째 원소부터/n 번째 원소부터.c
@@ -1,15 +1,10 @@
 #include <stdio.h>
+#include <stdbool.h>
 #include <stdlib.h>
 
+// num_list_len은 배열 num_list의 길이입니다.
 int* solution(int num_list[], size_t num_list_len, int n) {
-    
-    int result_len = num_list_len - (n - 1);
-    int* answer = (int*)malloc(sizeof(int) * result_len);
-
-    for (int i = 0; i < result_len; i++) 
-    {
-        answer[i] = num_list[n - 1 + i];
-    }
-
+    // return 값은 malloc 등 동적 할당을 사용해주세요. 할당 길이는 상황에 맞게 변경해주세요.
+    int* answer = (int*)malloc(1);
     return answer;
 }
