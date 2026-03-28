37024798a5d3099784724dde3a836ee1faf6fd7f
Gong SungWoo <mrgong0515@gmail.com>
2026-03-28 19:16:04 +0900

    [Bronze V] Title: 문자와 문자열, Time: 0 ms, Memory: 1112 KB -BaekjoonHub

---
+++ 백준/Bronze/27866. 문자와 문자열/문자와 문자열.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+
+int main(void)
+{
+    char S[1001]; // 문자열 선언
+    int i;
+    
+    scanf("%s", S);
+    scanf("%d", &i);
+    
+    printf("%c", S[i - 1]);
+    
+    return 0;
+}
